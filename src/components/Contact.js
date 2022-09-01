bfcdf604a10bc8c6873449dbe93aadf1f82b446a
Aryan <rajaryanhome@gmail.com>
2022-09-01 22:23:24 +0530

    refactored

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import contactImg from "../assets/img/contact-img.svg";
+
 import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+
 
 export const Contact = () => {
   const formInitialDetails = {
