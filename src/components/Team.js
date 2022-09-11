fc1593e79cf5a03b0c9a267015c0f512e3ee4a4e
Aryan <rajaryanhome@gmail.com>
2022-09-11 11:54:32 +0530

    Updated button text

--- src/components/Team.js
+++ src/components/Team.js
@@ -4,7 +4,7 @@ export const Team = () => {
 return (
   <>
   <div className="down" id="resume">Download My Resume by clicking on this button
-  <div ><a href={pdf} download="Aryanresume.pdf" className="resanc"><button class="button-87" role="button">Hit me to download!</button></a></div>
+  <div ><a href={pdf} className="resanc" target="_blank"><button class="button-87" role="button">download!</button></a></div>
   </div>
   </>
 )
